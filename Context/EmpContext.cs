44b199240e18d1bbda516b3536ba20f6e34721a7
ClasherHulk <krnskrwr4@gmail.com>
2021-03-15 16:09:33 +0530

    Second Commit

---
+++ Context/EmpContext.cs
@@ -0,0 +1,14 @@
+﻿using ExCodeFxGIT.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ExCodeFxGIT.Context
+{
+    public class EmpContext : DbContext
+    {
+        public DbSet<Emp> Emp { get; set; }
+    }
+}
